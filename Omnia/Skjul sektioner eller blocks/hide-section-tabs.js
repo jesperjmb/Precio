0ba83f695535a48ef1ad58db667089530cf83a8f
Jesper Borup <47186052+jesperjmb@users.noreply.github.com>
2022-02-02 11:21:52 +0100

    Update hide-section-tabs.js

--- Omnia/Skjul sektioner eller blocks/hide-section-tabs.js
+++ Omnia/Skjul sektioner eller blocks/hide-section-tabs.js
@@ -1,4 +1,6 @@
+/*FetchData can be removed or replaced with Omnia API*/
 function FetchData() {
+    /*Hide section tab with the class .tab-section and [hide-tabs] in the content*/
     $tabSections = document.querySelectorAll(".tab-section");    
     for ($tabSection of $tabSections) {
         if ($tabSection.innerText.includes('[hide-tabs]')) {
@@ -8,6 +10,7 @@ function FetchData() {
             $tabSection.style.display = "block";
         }
     }
+    /*Hide section tab with the class .tab-section and [hide-links] in the content. Originally created to specifically hide Process Links tab*/
     $hideLinks = document.querySelectorAll('.tab-section .v-tab:last-of-type')[0];   
     for ($tabSection of $tabSections) {
         if ($tabSection.innerText.includes('[hide-links]')) {
