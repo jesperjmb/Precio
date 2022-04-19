4cdebca7d05a67cf64dae326067f6b3ad4bdafab
Jesper Borup <47186052+jesperjmb@users.noreply.github.com>
2022-04-19 19:09:20 +0200

    Delete Skjul sektion tabs baseret på ugedag directory

--- Skjul sektion tabs baseret på ugedag/README.md
+++
@@ -1,2 +0,0 @@
-## Autovælg tab baseret på ugedag
-Scriptet gør det muligt at automatisk vælge et tab baseret på uge dag. Det kan eksempelvis bruges hvis en sektion har et tab for menuen for hver dag i ugen.
--- Skjul sektion tabs baseret på ugedag/script.js
+++
@@ -1,9 +0,0 @@
-//Autoselect tab baseret på ugedag
-function selectKantineDag() {
-  //Sæt getDay til at starte fra mandag i stedet for søndag
-  var dag = (new Date().getDay() + 6) % 7;
-  //Vælg tabs baseret på sektionens class
-  document.querySelectorAll(".kantine-sektion .v-slide-group__content .v-tab")[dag].click();
-}
-//Kør funktionen på onload
-window.onload = selectKantineDag();
